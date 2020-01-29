73353cd98a073701d2ec755ebaf9b453ec78cae1
asahina-dev <0.0.nn.ai@gmail.com>
2020-01-29 18:21:53 +0900

    feat:乱数を生成する

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    // 乱数生成
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
     // 入力値 : 可変変数である必要がある

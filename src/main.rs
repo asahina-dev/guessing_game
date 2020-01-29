b7b1a73a6d8ef80daf8a094a9c9a510a2d78d24f
asahina-dev <0.0.nn.ai@gmail.com>
2020-01-29 18:12:04 +0900

    feat: 入力した文字列を出力する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // 入力値 : 可変変数である必要がある
+    let mut guess = String::new();
+
+    // Stringでなければエラーを返す
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
